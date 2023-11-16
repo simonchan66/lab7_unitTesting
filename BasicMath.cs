404bee0c2d35d82f1b8ece78f044807a09da5218
simonchan66 <formul529@gmail.com>
2023-11-16 12:45:40 -0700

    first commit

---
+++ BasicMath.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7_Unit_Testing
+{
+    public class BasicMath
+    {
+    public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+
+        }
+        public double Subtract(double num1, double num2)
+        {
+            return num1 - num2;
+
+        }
+        public double Divide(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                throw new System.DivideByZeroException();
+            }
+            else { return num1 / num2; }
+
+        }
+        public double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Lab_7_Unit_Testing;
+
+BasicMath math = new BasicMath();
+Console.WriteLine("Please input the 1st number");
+double num1 = Double.Parse(Console.ReadLine());
+Console.WriteLine("Please input the 2nd number");
+double num2 = Double.Parse(Console.ReadLine());
+
+double sum = math.Add(num1, num2);
+Console.WriteLine("num1 + num2 = " + sum);
+double difference = math.Subtract(num1, num2);
+Console.WriteLine("num1 - num2 = " + difference);
+double quotient = math.Divide(num1, num2);
+Console.WriteLine("num1 / num2 = " + quotient);
+double product = math.Multiply(num1, num2);
+Console.WriteLine("num1 * num2 = " + product);
